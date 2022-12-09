b911c2dd0416d44006e98f12c2e7c761d5e42250
Jake Johnson <Jakejohnson21@rocketmail.com>
2022-12-09 03:22:15 -0600

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer__wrapper">
+      <p className="footer__copyright">Jake Johnson 2022 ©</p>
+    </footer>
+  );
+}
+
+export default Footer;
